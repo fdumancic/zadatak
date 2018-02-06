4621a0ae9f7a279e982f19cf90e39b8a6a0f8ff6
fdumancic <dumancic.filip@gmail.com>
2018-02-06 14:36:48 +0100

    4

---
+++ database/seeds/TagNoteTableSeeder.php
@@ -0,0 +1,45 @@
+<?php
+
+use Faker\Factory as Faker;
+use Faker\Provider\Base;
+use Faker\Provider\randomElement;
+use Illuminate\Database\Seeder;
+
+
+class TagNoteTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $faker = Faker::create('Notebook\Tag_note');
+
+        for($i=1; $i<=10; $i++){
+
+        	DB::table('tag_note')->insert([
+        		'note_id' => $faker->unique()->numberBetween($min = 1, $max = 10),
+        		'tag_id' => $faker->numberBetween($min = 1, $max = 4),
+        		'created_at' => \Carbon\Carbon::now(),
+        		'created_at' => \Carbon\Carbon::now(),
+
+
+        	]);
+        }
+        for($i=1; $i<=10; $i++){
+
+        	DB::table('tag_note')->insert([
+        		'note_id' => $faker->numberBetween($min = 1, $max = 10),
+        		'tag_id' => $faker->numberBetween($min = 1, $max = 4),
+        		'created_at' => \Carbon\Carbon::now(),
+        		'created_at' => \Carbon\Carbon::now(),
+
+
+        	]);
+        }
+
+
+    }
+}
---
+++ database/seeds/TagsTableSeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+use Faker\Factory as Faker;
+use Faker\Provider\Base;
+use Faker\Provider\randomElement;
+use Illuminate\Database\Seeder;
+
+
+class TagsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $faker = Faker::create('Notebook\Tags');
+
+        for($i=1; $i<=4; $i++){
+
+        	DB::table('tags')->insert([
+        		'tag' => $faker->unique()->randomElement(['PHP', 'HTML', 'JS', 'CSS']),
+        		'created_at' => \Carbon\Carbon::now(),
+        		'created_at' => \Carbon\Carbon::now(),
+
+        	]);
+        }
+        
+
+
+    }
+}
